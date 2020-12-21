875e4e6c33cda1ed54b07a43e2478432e8b14b64
elze <61447653+elzeXD@users.noreply.github.com>
2020-12-21 19:00:00 +0700

    Update base

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -7,13 +7,13 @@ WhatsAsena - Yusuf Usta
 */
 
 const Asena = require('../events');
-const {MessageType,Mimetype} = require('@adiwajshing/baileys');
+const { MessageType, Mimetype } = require('@adiwajshing/baileys');
 const translatte = require('translatte');
 const config = require('../config');
 //============================== CURRENCY =============================================
 const { exchangeRates } = require('exchange-rates-api');
 const ExchangeRatesError = require('exchange-rates-api/src/exchange-rates-error.js')
-//============================== TTS ==================================================
+    //============================== TTS ==================================================
 const fs = require('fs');
 const https = require('https');
 const googleTTS = require('google-translate-tts');
@@ -21,7 +21,7 @@ const googleTTS = require('google-translate-tts');
 //============================== YOUTUBE ==============================================
 const ytdl = require('ytdl-core');
 const ffmpeg = require('fluent-ffmpeg');
-const yts = require( 'yt-search' )
+const yts = require('yt-search')
 const got = require("got");
 const ID3Writer = require('browser-id3-writer');
 const SpotifyWebApi = require('spotify-web-api-node');
@@ -37,87 +37,86 @@ const Lang = Language.getString('scrapers');
 const wiki = require('wikijs').default;
 var gis = require('g-i-s');
 
-Asena.addCommand({pattern: 'trt(?: |$)(\\S*) ?(\\S*)', desc: Lang.TRANSLATE_DESC, usage: Lang.TRANSLATE_USAGE, fromMe: true}, (async (message, match) => {
+Asena.addCommand({ pattern: 'trt(?: |$)(\\S*) ?(\\S*)', desc: Lang.TRANSLATE_DESC, usage: Lang.TRANSLATE_USAGE, fromMe: true }, (async(message, match) => {
     if (!message.reply_message) {
-        return await message.reply(Lang.NEED_REPLY);
+        return await message.client.sendMessage(message.jid, Lang.NEED_REPLY, MessageType.text);
     }
 
-    ceviri = await translatte(message.reply_message.message, {from: match[1] === '' ? 'auto' : match[1], to: match[2] === '' ? config.LANG : match[2]});
+    ceviri = await translatte(message.reply_message.message, { from: match[1] === '' ? 'auto' : match[1], to: match[2] === '' ? config.LANG : match[2] });
     if ('text' in ceviri) {
-        return await message.reply('*▶️ ' + Lang.LANG + ':* ```' + (match[1] === '' ? 'auto' : match[1]) + '```\n'
-        + '*◀️ ' + Lang.FROM + '*: ```' + (match[2] === '' ? config.LANG : match[2]) + '```\n'
-        + '*🔎 ' + Lang.RESULT + ':* ```' + ceviri.text + '```');
+        return await message.reply('*▶️ ' + Lang.LANG + ':* ```' + (match[1] === '' ? 'auto' : match[1]) + '```\n' +
+            '*◀️ ' + Lang.FROM + '*: ```' + (match[2] === '' ? config.LANG : match[2]) + '```\n' +
+            '*🔎 ' + Lang.RESULT + ':* ```' + ceviri.text + '```');
     } else {
-        return await message.reply(Lang.TRANSLATE_ERROR)
+        return await message.client.sendMessage(message.jid, Lang.TRANSLATE_ERROR, MessageType.text)
     }
 }));
 
-Asena.addCommand({pattern: 'currency(?: ([0-9.]+) ([a-zA-Z]+) ([a-zA-Z]+)|$|(.*))', fromMe: true}, (async (message, match) => {
-    if(match[1] === undefined || match[2] == undefined || match[3] == undefined) {
-        return await message.reply(Lang.CURRENCY_ERROR);
+Asena.addCommand({ pattern: 'currency(?: ([0-9.]+) ([a-zA-Z]+) ([a-zA-Z]+)|$|(.*))', fromMe: true }, (async(message, match) => {
+    if (match[1] === undefined || match[2] == undefined || match[3] == undefined) {
+        return await message.client.sendMessage(message.jid, Lang.CURRENCY_ERROR, MessageType.text);
     }
     let opts = {
-        amount: parseFloat(match[1]).toFixed(2).replace(/\.0+$/,''),
+        amount: parseFloat(match[1]).toFixed(2).replace(/\.0+$/, ''),
         from: match[2].toUpperCase(),
         to: match[3].toUpperCase()
     }
     try {
         result = await exchangeRates().latest().symbols([opts.to]).base(opts.from).fetch()
-        result = parseFloat(result).toFixed(2).replace(/\.0+$/,'')
+        result = parseFloat(result).toFixed(2).replace(/\.0+$/, '')
         await message.reply(`\`\`\`${opts.amount} ${opts.from} = ${result} ${opts.to}\`\`\``)
-    }
-    catch(err) {
-        if (err instanceof ExchangeRatesError) 
-            await message.reply(Lang.INVALID_CURRENCY)
+    } catch (err) {
+        if (err instanceof ExchangeRatesError)
+            await message.client.sendMessage(message.jid, Lang.INVALID_CURRENCY, MessageType.text)
         else {
-            await message.reply(Lang.UNKNOWN_ERROR)
+            await message.client.sendMessage(message.jid, Lang.UNKNOWN_ERROR, MessageType.text)
             console.log(err)
         }
     }
 }));
 
-Asena.addCommand({pattern: 'tts (.*)', fromMe: true, desc: Lang.TTS_DESC}, (async (message, match) => {
-    if(match[1] === undefined || match[1] == "")
+Asena.addCommand({ pattern: 'tts (.*)', fromMe: true, desc: Lang.TTS_DESC }, (async(message, match) => {
+    if (match[1] === undefined || match[1] == "")
         return;
-    
-    let 
+
+    let
         LANG = config.LANG,
         ttsMessage = match[1],
         SPEED = 1.0
 
-    if(langMatch = match[1].match("\\{([a-z]{2})\\}")) {
+    if (langMatch = match[1].match("\\{([a-z]{2})\\}")) {
         LANG = langMatch[1]
         ttsMessage = ttsMessage.replace(langMatch[0], "")
-    } 
-    if(speedMatch = match[1].match("\\{([0].[0-9]+)\\}")) {
+    }
+    if (speedMatch = match[1].match("\\{([0].[0-9]+)\\}")) {
         SPEED = parseFloat(speedMatch[1])
         ttsMessage = ttsMessage.replace(speedMatch[0], "")
     }
-    
+
     var buffer = await googleTTS.synthesize({
         text: ttsMessage,
         voice: LANG
     });
-    await message.sendMessage(buffer, MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true});
+    await message.client.sendMessage(message.jid, buffer, MessageType.audio, { mimetype: Mimetype.mp4Audio, ptt: true });
 }));
 
-Asena.addCommand({pattern: 'ripaudio ?(.*)', fromMe: true, desc: Lang.SONG_DESC}, (async (message, match) => { 
-    if (match[1] === '') return await message.sendMessage(Lang.NEED_TEXT_SONG);    
+Asena.addCommand({ pattern: 'ripaudio ?(.*)', fromMe: true, desc: Lang.SONG_DESC }, (async(message, match) => {
+    if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_TEXT_SONG, MessageType.text);
     let arama = await yts(match[1]);
     arama = arama.all;
-    if(arama.length < 1) return await message.sendMessage(Lang.NO_RESULT);
-    var reply = await message.sendMessage(Lang.DOWNLOADING_SONG);
+    if (arama.length < 1) return await message.client.sendMessage(message.jid, Lang.NO_RESULT, MessageType.text);
+    var reply = await message.client.sendMessage(message.jid, Lang.DOWNLOADING_SONG, MessageType.text);
 
     let title = arama[0].title.replace(' ', '+');
     let stream = ytdl(arama[0].videoId, {
         quality: 'highestaudio',
     });
-    
+
     got.stream(arama[0].image).pipe(fs.createWriteStream(title + '.jpg'));
     ffmpeg(stream)
         .audioBitrate(320)
         .save('./' + title + '.mp3')
-        .on('end', async () => {
+        .on('end', async() => {
             const writer = new ID3Writer(fs.readFileSync('./' + title + '.mp3'));
             writer.setFrame('TIT2', arama[0].title)
                 .setFrame('TPE1', [arama[0].author.name])
@@ -128,74 +127,72 @@ Asena.addCommand({pattern: 'ripaudio ?(.*)', fromMe: true, desc: Lang.SONG_DESC}
                 });
             writer.addTag();
 
-            reply = await message.reply(Lang.UPLOADING_SONG);
-            await message.sendMessage(Buffer.from(writer.arrayBuffer), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: false});
+            reply = await message.client.sendMessage(message.jid, Lang.UPLOADING_SONG, MessageType.text);
+            await message.client.sendMessage(message.jid, Buffer.from(writer.arrayBuffer), MessageType.audio, { mimetype: Mimetype.mp4Audio, ptt: false });
         });
 }));
 
-Asena.addCommand({pattern: 'ripvideo ?(.*)', fromMe: true, desc: Lang.VIDEO_DESC}, (async (message, match) => { 
-    if (match[1] === '') return await message.sendMessage(Lang.NEED_VIDEO);    
-    
+Asena.addCommand({ pattern: 'ripvideo ?(.*)', fromMe: true, desc: Lang.VIDEO_DESC }, (async(message, match) => {
+    if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_VIDEO, MessageType.text);
+
     try {
-        var arama = await yts({videoId: ytdl.getURLVideoID(match[1])});
+        var arama = await yts({ videoId: ytdl.getURLVideoID(match[1]) });
     } catch {
-        return await message.sendMessage(Lang.NO_RESULT);
+        return await message.client.sendMessage(message.jid, Lang.NO_RESULT, MessageType.text);
     }
 
-    var reply = await message.reply(Lang.DOWNLOADING_VIDEO);
+    var reply = await message.client.sendMessage(message.jid, Lang.DOWNLOADING_VIDEO, MessageType.text);
 
-    var yt = ytdl(arama.videoId, {filter: format => format.container === 'mp4' && ['720p', '480p', '360p', '240p', '144p'].map(() => true)});
+    var yt = ytdl(arama.videoId, { filter: format => format.container === 'mp4' && ['720p', '480p', '360p', '240p', '144p'].map(() => true) });
     yt.pipe(fs.createWriteStream('./' + arama.videoId + '.mp4'));
 
-    yt.on('end', async () => {
-        await reply.delete();
-        reply = await message.reply(Lang.UPLOADING_VIDEO);
-        await message.sendMessage(fs.readFileSync('./' + arama.videoId + '.mp4'), MessageType.video, {mimetype: Mimetype.mp4});
-        await reply.delete();
+    yt.on('end', async() => {
+        reply = await message.client.sendMessage(message.jid, Lang.UPLOADING_VIDEO, MessageType.text);
+        await message.client.sendMessage(message.jid, fs.readFileSync('./' + arama.videoId + '.mp4'), MessageType.video, { mimetype: Mimetype.mp4 });
     });
 }));
 
-Asena.addCommand({pattern: 'yt ?(.*)', fromMe: true, desc: Lang.YT_DESC}, (async (message, match) => { 
-    if (match[1] === '') return await message.sendMessage(Lang.NEED_WORDS);    
-    var reply = await message.reply(Lang.GETTING_VIDEOS);
+Asena.addCommand({ pattern: 'yt ?(.*)', fromMe: true, desc: Lang.YT_DESC }, (async(message, match) => {
+    if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_WORDS, MessageType.text);
+    var reply = await message.client.sendMessage(message.jid, Lang.GETTING_VIDEOS, MessageType.text);
 
     try {
         var arama = await yts(match[1]);
     } catch {
-        return await message.sendMessage(Lang.NOT_FOUND);
+        return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
     }
-    
+
     var mesaj = '';
     arama.all.map((video) => {
         mesaj += '*' + video.title + '* - ' + video.url + '\n'
     });
 
-    await message.sendMessage(mesaj);
+    await message.client.sendMessage(message.jid, mesaj, MessageType.text);
     await reply.delete();
 }));
 
-Asena.addCommand({pattern: 'wiki ?(.*)', fromMe: true, desc: Lang.WIKI_DESC}, (async (message, match) => { 
-    if (match[1] === '') return await message.sendMessage(Lang.NEED_WORDS);    
-    var reply = await message.reply(Lang.SEARCHING);
+Asena.addCommand({ pattern: 'wiki ?(.*)', fromMe: true, desc: Lang.WIKI_DESC }, (async(message, match) => {
+    if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_WORDS, MessageType.text);
+    var reply = await message.client.sendMessage(message.jid, Lang.SEARCHING, MessageType.text);
 
     var arama = await wiki({ apiUrl: 'https://' + config.LANG + '.wikipedia.org/w/api.php' })
         .page(match[1]);
 
     var info = await arama.rawContent();
-    await message.reply(info);
+    await message.client.sendMessage(message.jid, info, MessageType.text);
     await reply.delete();
 }));
 
-Asena.addCommand({pattern: 'img ?(.*)', fromMe: true, desc: Lang.IMG_DESC}, (async (message, match) => { 
-    if (match[1] === '') return await message.sendMessage(Lang.NEED_WORDS);    
+Asena.addCommand({ pattern: 'img ?(.*)', fromMe: true, desc: Lang.IMG_DESC }, (async(message, match) => {
+    if (match[1] === '') return await message.client.sendMessage(message.jid, Lang.NEED_WORDS, MessageType.text);
 
-    gis(match[1], async (error, result) => {
+    gis(match[1], async(error, result) => {
         for (var i = 0; i < (result.length < 5 ? result.length : 5); i++) {
             var get = got(result[i].url);
             var stream = get.buffer();
-            
-            stream.then(async (image) => {
-                await message.sendMessage(image, MessageType.image);
+
+            stream.then(async(image) => {
+                await message.client.sendMessage(message.jid, image, MessageType.image);
             });
         }
 
